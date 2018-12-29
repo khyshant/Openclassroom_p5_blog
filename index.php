a1cbd9f85bccee028c25cbf065cd8a501b28d2e0
Anthony  Blanchard <anth.blanchard@gmail.com>
2018-12-29 01:20:35 +0100

    sensiolabs 3

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 $router = new App\classes\routeur\Router($_GET['url']);
 $router->get('/posts/:slug-:id',function($slug, $id) use($router){echo $router->url('post.show',['id'=> 1, 'slug' => 'salut-les-gens']);}, 'post.show')->with('id', '[0-9]+')->with('slug', '([a-z\-0-9])+');
 
-$router->get('/posts/:id',function($id){echo 'afficher l\'article ' .$id ;
+$router->get('/posts/:id',function($id){echo 'afficher l\'article ' .$id} ;
 
     $router->run();
 
