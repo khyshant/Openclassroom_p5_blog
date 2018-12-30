a580e16795f9dae994f881a36829bcdb45a6c97e
Anthony  Blanchard <anth.blanchard@gmail.com>
2018-12-31 00:53:00 +0100

    Update FrontController.php
    add user

--- src/controller/FrontController.php
+++ src/controller/FrontController.php
@@ -152,4 +152,46 @@ class FrontController{
             )
         );
     }
+	
+	public function validAccount(){
+
+        /*$securePwd = Tools::securePwd("BLO_a!b_2704");
+               $secureDob = Tools::valideDob('27-04-1977');
+               $test = new Users();
+               $test->createUser(1,'Blanchard', 'Anthony', 'anth.blanchard@gmail.com', '27-07-1977', $securePwd['pwd'], $securePwd['salt']);
+                */
+        $secureDob = Tools::valideDob($_POST['dob']);
+        $securePwd = Tools::securePwd($_POST['password']);
+
+        if($secureDob != false && $securePwd != false){
+            echo 'ici';
+            $account = new Users();
+            $account->createUser(2,$_POST['firstname'], $_POST['lastname'], $_POST['login'], $secureDob, $securePwd['pwd'], $securePwd['salt']);
+        }
+        echo 'ici1';
+        $erreur = "";
+        if($secureDob == false){
+            $erreur.=" - code erreur D100 - OB - ";
+        }
+        if($securePwd == false){
+            $erreur.="code erreur P100 - WD";
+        }
+        if($erreur !=""){
+            $tpl = new Template( 'src/view/frontend/' );
+            print $tpl->render( 'createAccountView', array(
+                'basedir' => $_SESSION['basedir'],
+                'title' => 'Vous pouvez créer un compte pour commenter les articles',
+                'menu' => $tpl::$frontMenu,
+                'erreur' => 'erreur durant le transfert du formulaire '.$erreur,
+                'contenu' => 'Cliquez dans la navigation pour choisir ce que vous souhaitez faire',
+                )
+            );
+        }
+        else{
+            echo 'ici';
+            // !!! attention au renvoi ==> voir fonction historic =1 de  michel
+            header('location:home');
+        }
+
+    }
 }
